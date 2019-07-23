dffb7f2cdd31eb4341298277d47505f78342201b
Florian Czarnecki <florian.czarnecki@adesso.de>
2019-07-23 17:42:42 +0200

    Style login page

--- frontend/lib/widgets/login.dart
+++ frontend/lib/widgets/login.dart
@@ -15,6 +15,13 @@ class _LoginState extends State<Login> {
   final TextEditingController _username = TextEditingController();
   final TextEditingController _password = TextEditingController();
 
+  final fieldDecoration = (String hint) => InputDecoration(
+        hintText: hint,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,31 +32,50 @@ class _LoginState extends State<Login> {
         builder: (BuildContext context) {
           return Form(
             key: _formKey,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  controller: _username,
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Can\'t be empty';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _password,
-                  obscureText: true,
-                  validator: (value) {
-                    return value.isEmpty ? 'Cannot be empty' : null;
-                  },
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    _login();
-                  },
-                  child: Text('Login'),
-                )
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _username,
+                      validator: (value) {
+                        return value.isEmpty
+                            ? 'Username cannot be empty'
+                            : null;
+                      },
+                      decoration: fieldDecoration('Username'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _password,
+                      obscureText: true,
+                      validator: (value) {
+                        return value.isEmpty
+                            ? 'Password cannot be empty'
+                            : null;
+                      },
+                      decoration: fieldDecoration('Password'),
+                    ),
+                  ),
+                  ButtonTheme(
+                    minWidth: 200,
+                    child: FlatButton(
+                      shape: StadiumBorder(),
+                      onPressed: () {
+                        _login(context);
+                      },
+                      color: Theme.of(context).primaryColor,
+                      textColor: Theme.of(context).secondaryHeaderColor,
+                      child: Text('Login'),
+                    ),
+                  )
+                ],
+              ),
             ),
           );
         },
@@ -57,14 +83,24 @@ class _LoginState extends State<Login> {
     );
   }
 
-  _login() async {
+  _login(BuildContext context) async {
     if (_formKey.currentState.validate()) {
       await storage.write(key: 'username', value: _username.text);
       await storage.write(key: 'password', value: _password.text);
-    }
-    var loggedIn = await auth.login();
-    if (loggedIn) {
-      Navigator.pushReplacementNamed(context, '/home');
+
+      var loggedIn = await auth.login();
+      if (loggedIn) {
+        Navigator.pushReplacementNamed(context, '/home');
+      } else {
+        Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Error loggin in'),
+            action: SnackBarAction(
+                label: 'Close',
+                onPressed: () => Scaffold.of(context).removeCurrentSnackBar()),
+          ),
+        );
+      }
     }
   }
 }
